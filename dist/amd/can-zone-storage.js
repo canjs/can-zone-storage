866ff58118d2cb75d6d7a7e300d4b7e2d3a00413
imasustink <austin@austinkurpuis.com>
2017-11-01 18:01:27 -0700

    1.0.1

---
+++ dist/amd/can-zone-storage.js
@@ -0,0 +1,22 @@
+/*can-zone-storage@1.0.0#can-zone-storage*/
+define(function (require, exports, module) {
+    module.exports = {
+        data: {},
+        getStore: function () {
+            var CanZone = window.CanZone || undefined;
+            return typeof CanZone === 'undefined' ? this.data : CanZone.current.data;
+        },
+        setItem: function (prop, value) {
+            var store = this.getStore();
+            store[prop] = value;
+        },
+        getItem: function (prop) {
+            var store = this.getStore();
+            return store[prop];
+        },
+        removeItem: function (prop) {
+            var store = this.getStore();
+            delete store[prop];
+        }
+    };
+});
